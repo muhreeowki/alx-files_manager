e0b39b5567bc0a97506b449a2247a0ed6dab4222
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-07-12 11:31:20 +0300

    Added getUser function to files controller.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,30 @@
 import { v4 as uuidv4 } from "uuid";
 import { promises as fs } from "fs";
 import { ObjectID } from "mongodb";
+import mime from "mime-types";
+import Queue from "bull";
 import dbClient from "../utils/db";
+import redisClient from "../utils/redis";
+
+const fileQueue = new Queue("fileQueue", "redis://127.0.0.1:6379");
 
 class FilesController {
+  static async getUser(request) {
+    const token = request.header("X-Token");
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection("users");
+      const idObject = new ObjectID(userId);
+      const user = await users.findOne({ _id: idObject });
+      if (!user) {
+        return null;
+      }
+      return user;
+    }
+    return null;
+  }
+
   static async postUpload(request, response) {
     const user = await FilesController.getUser(request);
     if (!user) {
@@ -23,6 +44,7 @@ class FilesController {
     if (type !== "folder" && !data) {
       return response.status(400).json({ error: "Missing data" });
     }
+
     const files = dbClient.db.collection("files");
     if (parentId) {
       const idObject = new ObjectID(parentId);
